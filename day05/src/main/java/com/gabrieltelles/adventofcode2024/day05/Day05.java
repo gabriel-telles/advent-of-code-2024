d461392b219d749786333f66268610a03837d17a
telles <gabrieltellesm@gmail.com>
2024-12-08 02:08:40 -0300

    Day05 Finish part one

--- day05/src/main/java/com/gabrieltelles/adventofcode2024/day05/Day05.java
+++ day05/src/main/java/com/gabrieltelles/adventofcode2024/day05/Day05.java
@@ -6,7 +6,9 @@ import com.google.common.collect.SetMultimap;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Day05 {
     private static final String RULES_PATH = "day05/src/main/resources/rules.csv";
@@ -16,16 +18,34 @@ public class Day05 {
     private static final String UPDATES_DELIMITER = ",";
 
     public static void main(String[] args) {
-        SetMultimap<String, String> rules = getRules();
-        List<List<String>> updates = getUpdates();
+        SetMultimap<String, String> rules = getRules(RULES_PATH);
+        List<List<String>> updates = getUpdates(UPDATES_PATH);
         if (rules == null || updates == null) return;
-        System.out.println(rules.get("44"));
-        System.out.println(updates.getFirst());
+
+        List<List<String>> validUpdates = updates.stream().filter(up -> updateRespectRules(up, rules)).toList();
+        int middlePageSum = validUpdates.stream().mapToInt(up -> Integer.parseInt(up.get((up.size()-1)/2))).sum();
+
+        System.out.println(middlePageSum);
     }
 
-    private static SetMultimap<String, String> getRules() {
+    static boolean updateRespectRules(List<String> update, SetMultimap<String, String> rules) {
+        ListIterator<String> iterator = update.listIterator(update.size());
+        List<String> successors = new LinkedList<>();
+        while (iterator.hasPrevious()) {
+            String element = iterator.previous();
+            for (var successor : successors) {
+                if (rules.get(element).contains(successor)) {
+                    return false;
+                }
+            }
+            successors.add(element);
+        }
+        return true;
+    }
+
+    static SetMultimap<String, String> getRules(String path) {
         SetMultimap<String, String> rules = HashMultimap.create();
-        try (var lines = Files.lines(Paths.get(RULES_PATH))) {
+        try (var lines = Files.lines(Paths.get(path))) {
             lines.map(line -> line.split(RULES_DELIMITER))
                     .forEach(rule -> rules.put(rule[1], rule[0]));
         } catch (IOException e) {
@@ -35,9 +55,9 @@ public class Day05 {
         return rules;
     }
 
-    private static List<List<String>> getUpdates() {
+    static List<List<String>> getUpdates(String path) {
         List<List<String>> updates;
-        try (var lines = Files.lines(Paths.get(UPDATES_PATH))) {
+        try (var lines = Files.lines(Paths.get(path))) {
             updates = lines.map(line -> List.of(line.split(UPDATES_DELIMITER))).toList();
         } catch (IOException e) {
             System.out.println("Failed to load " + e.getMessage());
---
+++ day05/src/test/java/com/gabrieltelles/adventofcode2024/day05/Day05Test.java
@@ -0,0 +1,22 @@
+package com.gabrieltelles.adventofcode2024.day05;
+
+import com.google.common.collect.SetMultimap;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Day05Test {
+
+    @Test
+    void shouldGetMiddlePageSum() {
+        SetMultimap<String, String> rules = Day05.getRules("src/test/resources/exampleRules.csv");
+        List<List<String>> updates = Day05.getUpdates("src/test/resources/exampleUpdates.csv");
+        if (rules == null || updates == null) return;
+
+        List<List<String>> validUpdates = updates.stream().filter(up -> Day05.updateRespectRules(up, rules)).toList();
+        int middlePageSum = validUpdates.stream().mapToInt(up -> Integer.parseInt(up.get((up.size()-1)/2))).sum();
+
+        Assertions.assertEquals(143, middlePageSum);
+    }
+}
