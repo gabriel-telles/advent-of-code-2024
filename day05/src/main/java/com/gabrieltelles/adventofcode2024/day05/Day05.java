10dff67043dc808c6d0b4390fbed0db018568e4e
telles <gabrieltellesm@gmail.com>
2024-12-08 01:22:38 -0300

    Day05 use SetMultimap for rules

--- day05/src/main/java/com/gabrieltelles/adventofcode2024/day05/Day05.java
+++ day05/src/main/java/com/gabrieltelles/adventofcode2024/day05/Day05.java
@@ -1,22 +1,30 @@
 package com.gabrieltelles.adventofcode2024.day05;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class Day05 {
-    private static final String INPUT_PATH = "day05/src/main/resources/rules.csv";
-    private static final String DELIMITER = "\\|";
+    private static final String RULES_PATH = "day05/src/main/resources/rules.csv";
+    private static final String RULES_DELIMITER = "\\|";
     public static void main(String[] args) {
-        List<List<Integer>> rules;
-        try (var lines = Files.lines(Paths.get(INPUT_PATH))) {
-            rules = lines.map(line -> Stream.of(line.split(DELIMITER)).map(Integer::parseInt).toList()).toList();
-        } catch (IOException _) {
-            System.out.println("Failed to open file");
-            return;
+        SetMultimap<String, String> rules = getRules();
+        if (rules == null) return;
+        System.out.println(rules.get("44"));
+    }
+
+    private static SetMultimap<String, String> getRules() {
+        SetMultimap<String, String> rules = HashMultimap.create();
+        try (var lines = Files.lines(Paths.get(RULES_PATH))) {
+            lines.map(line -> line.split(RULES_DELIMITER))
+                    .forEach(rule -> rules.put(rule[1], rule[0]));
+        } catch (IOException e) {
+            System.out.println("Failed to load " + e.getMessage());
+            return null;
         }
-        System.out.println(rules.getFirst().getLast());
+        return rules;
     }
 }
