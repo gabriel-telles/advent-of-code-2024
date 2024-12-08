b00283b9fa2e215a65177ed4d2b4ae21ac5f5c10
telles <gabrieltellesm@gmail.com>
2024-12-08 01:28:25 -0300

    Day05 read updates

--- day05/src/main/java/com/gabrieltelles/adventofcode2024/day05/Day05.java
+++ day05/src/main/java/com/gabrieltelles/adventofcode2024/day05/Day05.java
@@ -6,14 +6,21 @@ import com.google.common.collect.SetMultimap;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Day05 {
     private static final String RULES_PATH = "day05/src/main/resources/rules.csv";
     private static final String RULES_DELIMITER = "\\|";
+
+    private static final String UPDATES_PATH = "day05/src/main/resources/updates.csv";
+    private static final String UPDATES_DELIMITER = ",";
+
     public static void main(String[] args) {
         SetMultimap<String, String> rules = getRules();
-        if (rules == null) return;
+        List<List<String>> updates = getUpdates();
+        if (rules == null || updates == null) return;
         System.out.println(rules.get("44"));
+        System.out.println(updates.getFirst());
     }
 
     private static SetMultimap<String, String> getRules() {
@@ -26,5 +33,16 @@ public class Day05 {
             return null;
         }
         return rules;
+    }
+
+    private static List<List<String>> getUpdates() {
+        List<List<String>> updates;
+        try (var lines = Files.lines(Paths.get(UPDATES_PATH))) {
+            updates = lines.map(line -> List.of(line.split(UPDATES_DELIMITER))).toList();
+        } catch (IOException e) {
+            System.out.println("Failed to load " + e.getMessage());
+            return null;
+        }
+        return updates;
     }
 }
